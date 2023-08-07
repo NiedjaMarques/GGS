edf6ee49c7866fa2b7031b7bb951753f5cad435f
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-08-07 12:24:48 -0300

    Tornando o site responsivo e dinâmico

--- Javascript/arquivo01.js
+++ Javascript/arquivo01.js
@@ -1,10 +1,17 @@
 function MenuNavBar() { //função chamada
-    const mostrarMenu = document.getElementById('menu') // Pega o elemento com ID "menu" e armazena na variavel
+    const menu = document.getElementById('menu') // Pega o elemento com ID "menu" e armazena na variavel
 
     // Verifica se o menu esta oculto no HTML usando a função "window.getComputedStyle(menu)"
     if (window.getComputedStyle(menu).display === 'none') { // Se o menu não estiver sendo exibido...
         menu.style.display = 'block' // ...quando for clicado ele mostra as opções
+
+        
+       // menu.classList.add('flex-col'); // Adiciona a classe "flex-col" para exibir como coluna
+        //menu.classList.remove('hidden'); // Remove a classe "hidden" para tornar o menu visível
+
+
     } else {
         menu.style.display = 'none'; // ...quando clicado novamente, esconde as opções
     }
 }
+
