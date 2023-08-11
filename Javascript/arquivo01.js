4602143490b0f2c8430c13edac454aef00e4f26c
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-08-11 15:26:02 -0300

    Reescrevendo Nav

--- Javascript/arquivo01.js
+++ Javascript/arquivo01.js
@@ -1,17 +1,29 @@
-function MenuNavBar() { //função chamada
-    const menu = document.getElementById('menu') // Pega o elemento com ID "menu" e armazena na variavel
+function MenuNavBar() {
+    const menu = document.getElementById("menu");
 
-    // Verifica se o menu esta oculto no HTML usando a função "window.getComputedStyle(menu)"
-    if (window.getComputedStyle(menu).display === 'none') { // Se o menu não estiver sendo exibido...
-        menu.style.display = 'block' // ...quando for clicado ele mostra as opções
+    let iconeAtual = 1;
+
+}
+
+
+
+
+
+
+// function MenuNavBar(e) { //função chamada
+//     const menu = document.getElementById('menu') // Pega o elemento com ID "menu" e armazena na variavel
+
+//     // Verifica se o menu esta oculto no HTML usando a função "window.getComputedStyle(menu)"
+//     if (window.getComputedStyle(menu).display === 'none') { // Se o menu não estiver sendo exibido...
+//         menu.style.display = 'block' // ...quando for clicado ele mostra as opções
 
         
-       //menu.style.flexDirection = 'column'; // Adiciona a classe "flex-col" para exibir como coluna
-        //menu.classList.remove('hidden'); // Remove a classe "hidden" para tornar o menu visível
+//        //menu.style.flexDirection = 'column'; // Adiciona a classe "flex-col" para exibir como coluna
+//         //menu.classList.remove('hidden'); // Remove a classe "hidden" para tornar o menu visível
 
 
-    } else {
-        menu.style.display = 'none'; // ...quando clicado novamente, esconde as opções
-    }
-}
+//     } else {
+//         menu.style.display = 'none'; // ...quando clicado novamente, esconde as opções
+//     }
+// }
 
