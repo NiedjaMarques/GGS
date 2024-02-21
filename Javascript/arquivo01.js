faf47c4b9011064c270022020eb6ec93dd68b00f
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2024-02-21 18:17:31 -0300

    ajuste menu navBar

--- Javascript/arquivo01.js
+++ Javascript/arquivo01.js
@@ -1,34 +1,50 @@
 //Acontece quando a pagina é carregada
-document.addEventListener("DOMContentLoaded", function(){
-    // Esconder o carregamento e mostrar o conteúdo
-    const carregamentoPagina = document.getElementById("carregamentoPagina");
-    const conteudoPagina = document.getElementById("conteudoPagina");
+document.addEventListener("DOMContentLoaded", function () {
+  // Esconder o carregamento e mostrar o conteúdo
+  const carregamentoPagina = document.getElementById("carregamentoPagina");
+  const conteudoPagina = document.getElementById("conteudoPagina");
 
-    carregamentoPagina.style.display = "block";
-    
-    setTimeout(function(){
-        carregamentoPagina.style.display = "none";
-        conteudoPagina.style.display = "block";
-    }, 450);
+  carregamentoPagina.style.display = "block";
+
+  setTimeout(function () {
+    carregamentoPagina.style.display = "none";
+    conteudoPagina.style.display = "block";
+  }, 350);
 });
 
-//Acontece quando o usuário rola a tela 
-window.addEventListener('scroll', function(){
-    const header = document.querySelector('#header');
-    if (window.scrollY > 0) {
-        addClass(header, ['bg-white', 'shadow-lg']); // Adiciona classes usando função
-    } else {
-        removeClass(header, ['bg-white', 'shadow-lg']); // Remove classes usando função
-    }
+//Acontece quando o usuário rola a tela
+document.addEventListener("scroll", function () {
+  const header = document.getElementById("header");
+  if (window.scrollY > 0) {
+    header.classList.add("bg-white", "shadow-lg");
+    header.style.height = "80px";
+  } else {
+    header.classList.remove("bg-white", "shadow-lg");
+    header.style.height = "96px";
+  }
 });
 
-// Abre o menu de navegação
-function openNav() {
-    document.querySelector("#myNav").style.width = "100%";
-    // document.querySelector("#open-btn").style.display = "none";
+//Fecha o menu de navegação
+function closeNav() {    
+    var menu = document.getElementById("myNav");
+    var openIcon = document.getElementById("open-menu");
+    var closeIcon = document.getElementById("close-menu");
+    var nav = document.getElementById("nav");
+
+    menu.style.width = "0";
+    openIcon.style.display = "block";
+    closeIcon.style.display = "none";
+    // nav.style.backgroundColor = 'white ';//
+    // nav.style.transition = "all 2s ease"
 }
 
-// Fecha o menu de navegação
-function closeNav() {
-    document.querySelector("#myNav").style.width = "0";
+//Abre o menu de navegação
+function openNav() {
+    var menu = document.getElementById("myNav");
+    var openIcon = document.getElementById("open-menu");
+    var closeIcon = document.getElementById("close-menu");
+
+    menu.style.width = "100%";
+    openIcon.style.display = "none";
+    closeIcon.style.display = "block";
 }
