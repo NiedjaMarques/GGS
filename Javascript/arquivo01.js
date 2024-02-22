9f738bdd25bdf841c643c956369ca360d5135c5a
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2024-02-22 18:44:55 -0300

    Ajuste dos menu's

--- Javascript/arquivo01.js
+++ Javascript/arquivo01.js
@@ -17,34 +17,52 @@ document.addEventListener("scroll", function () {
   const header = document.getElementById("header");
   if (window.scrollY > 0) {
     header.classList.add("bg-white", "shadow-lg");
-    header.style.height = "80px";
+    //header.style.height = "80px";
   } else {
     header.classList.remove("bg-white", "shadow-lg");
-    header.style.height = "96px";
+    //header.style.height = "96px";
   }
 });
 
 //Fecha o menu de navegação
 function closeNav() {    
-    var menu = document.getElementById("myNav");
+    var menu = document.getElementById("showNave");
     var openIcon = document.getElementById("open-menu");
     var closeIcon = document.getElementById("close-menu");
-    var nav = document.getElementById("nav");
+    let myNav = document.getElementById("myNav");
 
     menu.style.width = "0";
+
+    myNav.style.width = "0";
+
+    
+    
+    myNav.style.transition = "all 0.5s";
+
     openIcon.style.display = "block";
     closeIcon.style.display = "none";
-    // nav.style.backgroundColor = 'white ';//
-    // nav.style.transition = "all 2s ease"
 }
 
 //Abre o menu de navegação
 function openNav() {
-    var menu = document.getElementById("myNav");
+    var menu = document.getElementById("showNave");
     var openIcon = document.getElementById("open-menu");
     var closeIcon = document.getElementById("close-menu");
+     let myNav = document.getElementById("myNav");   
 
     menu.style.width = "100%";
-    openIcon.style.display = "none";
+
+    setTimeout(function (){
+      myNav.style.width = "280px";
+      myNav.style.transition = "all 0.5s";
+
+      //menu.classList.add('width-100');
+    }, 1500);
+
+    
+        
+    
+    openIcon.style.display = "none";    
+    closeIcon.style.opacity = "1";
     closeIcon.style.display = "block";
 }
